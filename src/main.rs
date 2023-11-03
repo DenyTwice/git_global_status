5d93a8762f36696d10aff83edd0ad8fe7cc33551
DenyTwice <ivinjabraham@gmail.com>
2023-11-03 11:04:19 +0530

    adds default argument and refactors has_unpushed_commits, probably final commit

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::env;
+use std::fs;
+use std::fs::File;
 use std::io::Error as IOError;
 use std::io::ErrorKind;
+use std::io::Write;
 use std::path::{Path, PathBuf};
 use std::process::exit;
 
 use git2::{Repository, StatusOptions, Error};
 
-const USAGE: &str = "Usage: ggs [PATH_TO_DIRECTORY]";
+const USAGE: &str = "Usage: ggs [-d] [input]";
 const ALL_GOOD: &str = "All good!";
 const UNPUSHED_COMMITS_MSG: &str = "Directories with unpushed commits:";
 const STAGED_CHANGES_MSG: &str = "Directories with staged changes:";
@@ -22,9 +25,25 @@ enum GitStatus {
 fn main() {
     let args: Vec<String> = env::args().collect();
     match args.len() {
+        1 => {
+            let default_directory = match get_default_directory() {
+                Ok(dir) => dir,
+                Err(_) => {
+                    println!("No defaults specified.\n{}", USAGE);
+                    exit(1);
+                }
+            };
+            driver(&default_directory);
+        }
         2 => { 
             driver(&args[1]);
         }
+        3 => {
+            match set_default_directory(&args[2]) {
+                Ok(()) => driver(&args[2]),
+                Err(e) => println!("Error: {}. Could not set default directory.", e),
+            }
+        }   
         _ => {
             println!("{}", USAGE);
         }
@@ -41,7 +60,7 @@ fn driver(path_string: &str) {
                 ErrorKind::PermissionDenied => println!("Permission to access directory denied."),
                 _ => println!("Error, could not read directory. Please check if given path points to a directory"),
             }
-            exit(0);
+            exit(1);
         }
     };
     
@@ -111,6 +130,11 @@ fn check_status(repo: Repository) -> Result<GitStatus, Error> {
     for entry in statuses.iter() {
         let status = entry.status();
 
+
+        if let Ok(true) = has_commits_not_pushed(&repo) {
+            return Ok(GitStatus::UnpushedCommits);
+        }
+
         if status.intersects(git2::Status::INDEX_NEW | git2::Status::INDEX_MODIFIED | git2::Status::INDEX_DELETED) {
             return Ok(GitStatus::Staged);
         }
@@ -118,11 +142,6 @@ fn check_status(repo: Repository) -> Result<GitStatus, Error> {
         if status.intersects(git2::Status::WT_MODIFIED | git2::Status::WT_DELETED) {
             return Ok(GitStatus::Modified);
         }
-
-        if has_commits_not_pushed(&repo) {
-            return Ok(GitStatus::UnpushedCommits);
-        }
-
     }
 
     Ok(GitStatus::NoChanges)
@@ -137,37 +156,48 @@ fn print_status(directories: &[String], message: &str) {
     }
 }
 
-// refactor so your eyes don't bleed when you look at this in the future
-fn has_commits_not_pushed(repo: &Repository) -> bool {
-    let head = match repo.head() {
-        Ok(head) => head,
-        Err(_) => return false,
+fn has_commits_not_pushed(repo: &Repository) -> Result<bool, Error> {
+    let head = repo.head()?;
+    let branch_name = head.shorthand().ok_or(Error::from_str("Branch name not found"))?;
+    let local_branch = repo.find_branch(branch_name, git2::BranchType::Local)?;
+    let upstream_branch = local_branch.upstream()?;
+
+    let local_oid = repo.refname_to_id(local_branch.get().name().ok_or(Error::from_str("Local branch name not found"))?)?;
+    let upstream_oid = repo.refname_to_id(upstream_branch.get().name().ok_or(Error::from_str("Upstream branch name not found"))?)?;
+
+    Ok(local_oid != upstream_oid)
+}
+
+fn set_default_directory(path: &String) -> Result<(), IOError> {
+ 
+    let home = match env::var("HOME") {
+        Ok(val) => val,
+        Err(e) => panic!("Couldn't read HOME environment variable ({})", e),
     };
 
-    let branch_name = match head.shorthand() {
-        Some(name) => name,
-        None => return false,
+    let mut config_path = PathBuf::from(home);
+    config_path.push(".config/ggs/config.txt");
+
+    if let Some(dir) = config_path.parent() {
+        std::fs::create_dir_all(dir)?;
+    } 
+
+    let mut file = File::create(&config_path)?;
+    file.write_all(path.as_bytes())?;
+    Ok(())
+}
+
+fn get_default_directory() -> Result<String, IOError> {
+    let home = match env::var("HOME") {
+        Ok(val) => val,
+        Err(e) => panic!("Couldn't read HOME environment variable ({})", e),
     };
 
-    let local_branch = match repo.find_branch(branch_name, git2::BranchType::Local) {
-        Ok(branch) => branch,
-        Err(_) => return false,
-    };
+    // Create a path using the HOME variable
+    let mut config_path = PathBuf::from(home);
+    config_path.push(".config/ggs/config.txt");
 
-    let upstream_branch = match local_branch.upstream() {
-        Ok(branch) => branch,
-        Err(_) => return false,
-    };
-
-    let local_oid = match repo.refname_to_id(local_branch.get().name().unwrap_or("")) {
-        Ok(oid) => oid,
-        Err(_) => return false,
-    };
-
-    let upstream_oid = match repo.refname_to_id(upstream_branch.get().name().unwrap_or("")) {
-        Ok(oid) => oid,
-        Err(_) => return false,
-    };
-
-    local_oid != upstream_oid
+    let contents = fs::read_to_string(config_path)?;
+    
+    Ok(contents)
 }
