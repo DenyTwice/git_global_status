846087cfb74c068071251d1f2384b322504ebf13
DenyTwice <ivinjabraham@gmail.com>
2023-11-19 20:32:22 +0530

    adds function to parse input into command type

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::process::exit;
 
 use git2::{Repository, StatusOptions, Error};
 
-const USAGE: &str = "Usage: ggs [-d] <input>";
+const USAGE: &str = "Usage: ggs [OPTIONS] <path_to_directory>";
 const ALL_GOOD: &str = "All good!";
 const UNPUSHED_COMMITS_MSG: &str = "Directories with unpushed commits:";
 const STAGED_CHANGES_MSG: &str = "Directories with staged changes:";
@@ -19,35 +19,63 @@ enum GitStatus {
     UnpushedCommits
 }
 
+enum CommandType<'a> {
+    Default,
+    DirectorySpecified(&'a str),
+    Help,
+    Add,
+    Invalid,
+}
+
+fn parse_input(mut args: Vec<&str>) -> CommandType {
+    args.swap_remove(0); // remove ggs
+    
+    if args.is_empty() {
+        return CommandType::Default;
+    }
+
+    if args.len() == 1 {
+        match args[0] {
+            "--help" => return CommandType::Help,
+            dir => return CommandType::DirectorySpecified(dir)
+        }
+    };
+
+    if args[0] == "--add" || args[1] == "--add" {
+        return CommandType::Add; 
+    }
+
+    if args.len() > 2 {
+        return CommandType::Invalid; 
+    }
+
+    CommandType::Invalid
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    match args.as_slice() {
-        [default] => {
-            let default_directory = match get_default_directory() {
-                Ok(dir) => dir,
-                Err(_) => {
-                    println!("No defaults specified.\n{}", USAGE);
-                    exit(1);
-                }
-            };
-            driver(&default_directory);
-        }
-        [default, directory] => { 
-            driver(&args[1]);
-        }
-        [_, option, _] if option == &String::from("-d") => {
-            match set_default_directory(&args[2]) {
-                Ok(()) => driver(&args[2]),
-                Err(e) => println!("Error: {}. Could not set default directory.", e),
-            }
-        }   
-        _ => {
-            println!("{}", USAGE);
-        }
+    let args: Vec<String> = env::args()
+        .collect();
+
+    let args_str: Vec<&str> = args.iter()
+        .map(|s| s.as_str())
+        .collect();
+
+    let command_type: CommandType = parse_input(args_str);
+
+    match command_type {
+        CommandType::Help | CommandType::Invalid => help(),
+        CommandType::DirectorySpecified(dir) => default(Some(dir)),
+        CommandType::Add => add_to_config(),
+        CommandType::Default => default(None)
     }
 }
 
-fn driver(path_string: &str) {
+
+fn help() {
+    println!("lmao you loser");
+}
+
+fn default(path_string: Option<&str>) {
     let path = Path::new(&path_string);
     let directories: Vec<PathBuf> = match list_directories(&PathBuf::from(path)) {
         Ok(dirs) => dirs,
@@ -98,126 +126,129 @@ fn driver(path_string: &str) {
 
 }
 
-fn list_directories(path: &PathBuf) -> Result<Vec<PathBuf>,IOError>{
-
-    let mut directories: Vec<PathBuf> = Vec::new();
-    for entry in path.read_dir()? {
-        if let Ok(dir) = entry {
-            if  dir.path().is_dir() {
-                directories.push(dir.path());
-            }
-        }
-    }
-    
-    Ok(directories)
-}
-    
-fn check_status(repo: Repository) -> Result<GitStatus, Error> {
-
-    let mut opts = StatusOptions::new();
-    opts.show(git2::StatusShow::IndexAndWorkdir);
-    opts.include_untracked(true);
-    opts.recurse_untracked_dirs(true);
-
-    let statuses = match repo.statuses(Some(&mut opts)) {
-        Ok(status) => status,
-        Err(error) => return Err(error),
-    };
-
-    for entry in statuses.iter() {
-        let status = entry.status();
-
-
-        if has_commits_not_pushed(&repo) {
-            return Ok(GitStatus::UnpushedCommits);
-        }
-
-        if status.intersects(git2::Status::INDEX_NEW | git2::Status::INDEX_MODIFIED | git2::Status::INDEX_DELETED) {
-            return Ok(GitStatus::Staged);
-        }
-
-        if status.intersects(git2::Status::WT_MODIFIED | git2::Status::WT_DELETED) {
-            return Ok(GitStatus::Modified);
-        }
-    }
-
-    Ok(GitStatus::NoChanges)
+fn add_to_config() {
 }
 
-fn print_status(directories: &[String], message: &str) {
-    if !directories.is_empty() {
-        println!("{}", message);
-        for directory in directories {
-            println!("  * {}", directory);
-        }
-    }
-}
-
-
-fn has_commits_not_pushed(repo: &Repository) -> bool {
-    let head = match repo.head() {
-        Ok(head) => head,
-        Err(_) => return false,
-    };
-
-    let branch_name = match head.shorthand() {
-        Some(name) => name,
-        None => return false,
-    };
-
-    let local_branch = match repo.find_branch(branch_name, git2::BranchType::Local) {
-        Ok(branch) => branch,
-        Err(_) => return false,
-    };
-
-    let upstream_branch = match local_branch.upstream() {
-        Ok(branch) => branch,
-        Err(_) => return false,
-    };
-
-    let local_oid = match repo.refname_to_id(local_branch.get().name().unwrap_or("")) {
-        Ok(oid) => oid,
-        Err(_) => return false,
-    };
-
-    let upstream_oid = match repo.refname_to_id(upstream_branch.get().name().unwrap_or("")) {
-        Ok(oid) => oid,
-        Err(_) => return false,
-    };
-
-    local_oid != upstream_oid
-}
-
-fn set_default_directory(path: &String) -> Result<(), IOError> {
- 
-    let home = match env::var("HOME") {
-        Ok(val) => val,
-        Err(e) => panic!("Couldn't read HOME environment variable ({})", e),
-    };
-
-    let mut config_path = PathBuf::from(home);
-    config_path.push(".config/ggs/config.txt");
-
-    if let Some(dir) = config_path.parent() {
-        std::fs::create_dir_all(dir)?;
-    } 
-
-    let mut file = std::fs::File::create(&config_path)?;
-    file.write_all(path.as_bytes())?;
-    Ok(())
-}
-
-fn get_default_directory() -> Result<String, IOError> {
-    let home = match env::var("HOME") {
-        Ok(val) => val,
-        Err(e) => panic!("Couldn't read HOME environment variable ({})", e),
-    };
-
-    // Create a path using the HOME variable
-    let mut config_path = PathBuf::from(home);
-    config_path.push(".config/ggs/config.txt");
-
-    let contents = std::fs::read_to_string(config_path)?;
-    
-    Ok(contents)
-}
+//fn list_directories(path: &PathBuf) -> Result<Vec<PathBuf>,IOError>{
+//
+//    let mut directories: Vec<PathBuf> = Vec::new();
+//    for entry in path.read_dir()? {
+//        if let Ok(dir) = entry {
+//            if  dir.path().is_dir() {
+//                directories.push(dir.path());
+//            }
+//        }
+//    }
+//    
+//    Ok(directories)
+//}
+//    
+//fn check_status(repo: Repository) -> Result<GitStatus, Error> {
+//
+//    let mut opts = StatusOptions::new();
+//    opts.show(git2::StatusShow::IndexAndWorkdir);
+//    opts.include_untracked(true);
+//    opts.recurse_untracked_dirs(true);
+//
+//    let statuses = match repo.statuses(Some(&mut opts)) {
+//        Ok(status) => status,
+//        Err(error) => return Err(error),
+//    };
+//
+//    for entry in statuses.iter() {
+//        let status = entry.status();
+//
+//
+//        if has_commits_not_pushed(&repo) {
+//            return Ok(GitStatus::UnpushedCommits);
+//        }
+//
+//        if status.intersects(git2::Status::INDEX_NEW | git2::Status::INDEX_MODIFIED | git2::Status::INDEX_DELETED) {
+//            return Ok(GitStatus::Staged);
+//        }
+//
+//        if status.intersects(git2::Status::WT_MODIFIED | git2::Status::WT_DELETED) {
+//            return Ok(GitStatus::Modified);
+//        }
+//    }
+//
+//    Ok(GitStatus::NoChanges)
+//}
+//
+//fn print_status(directories: &[String], message: &str) {
+//    if !directories.is_empty() {
+//        println!("{}", message);
+//        for directory in directories {
+//            println!("  * {}", directory);
+//        }
+//    }
+//}
+//
+//
+//fn has_commits_not_pushed(repo: &Repository) -> bool {
+//    let head = match repo.head() {
+//        Ok(head) => head,
+//        Err(_) => return false,
+//    };
+//
+//    let branch_name = match head.shorthand() {
+//        Some(name) => name,
+//        None => return false,
+//    };
+//
+//    let local_branch = match repo.find_branch(branch_name, git2::BranchType::Local) {
+//        Ok(branch) => branch,
+//        Err(_) => return false,
+//    };
+//
+//    let upstream_branch = match local_branch.upstream() {
+//        Ok(branch) => branch,
+//        Err(_) => return false,
+//    };
+//
+//    let local_oid = match repo.refname_to_id(local_branch.get().name().unwrap_or("")) {
+//        Ok(oid) => oid,
+//        Err(_) => return false,
+//    };
+//
+//    let upstream_oid = match repo.refname_to_id(upstream_branch.get().name().unwrap_or("")) {
+//        Ok(oid) => oid,
+//        Err(_) => return false,
+//    };
+//
+//    local_oid != upstream_oid
+//}
+//
+//fn add(path: &String) -> Result<(), IOError> {
+// 
+//    let home = match env::var("HOME") {
+//        Ok(val) => val,
+//        Err(e) => panic!("Couldn't read HOME environment variable ({})", e),
+//    };
+//
+//    let mut config_path = PathBuf::from(home);
+//    config_path.push(".config/ggs/config.txt");
+//
+//    if let Some(dir) = config_path.parent() {
+//        std::fs::create_dir_all(dir)?;
+//    } 
+//
+//    let mut file = std::fs::File::create(&config_path)?;
+//    file.write_all(path.as_bytes())?;
+//    Ok(())
+//}
+//
+//fn get_default_directory() -> Result<String, IOError> {
+//    let home = match env::var("HOME") {
+//        Ok(val) => val,
+//        Err(e) => panic!("Couldn't read HOME environment variable ({})", e),
+//    };
+//
+//    // Create a path using the HOME variable
+//    let mut config_path = PathBuf::from(home);
+//    config_path.push(".config/ggs/config.txt");
+//
+//    let contents = std::fs::read_to_string(config_path)?;
+//    
+//    Ok(contents)
+//}
